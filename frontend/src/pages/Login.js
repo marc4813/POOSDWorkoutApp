dc13a27a2680ae853fe2de50166db6ca384b70b9
Arwin Nimityongskul <arwinnimit@hotmail.com>
2023-04-22 18:14:01 -0400

    Login and Register Sample Page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
 const Login = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const username = event.target.username.value;
+    const password = event.target.password.value;
+    console.log('Login submitted:');
+    console.log('Username:', username);
+    console.log('Password:', password);
+    // perform login action with username and password
+  };
+
   return (
     <div>
-      <h1>Hello This is Login Page</h1>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username"/>
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password"/>
+        </div>
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 };
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,9 +1,35 @@
 import React from 'react';
 
 const Register = () => {
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // perform registration action with name, username, password, and confirm password
+    console.log('Registration submitted:');
+  };
+
   return (
     <div>
-      <h1>Hello This Is Register Page</h1>
+      <h1>Register</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name"/>
+        </div>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username"/>
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password"/>
+        </div>
+        <div>
+          <label htmlFor="confirmPassword">Confirm Password:</label>
+          <input type="password" id="confirmPassword"/>
+        </div>
+        <button type="submit">Register</button>
+      </form>
     </div>
   );
 };
