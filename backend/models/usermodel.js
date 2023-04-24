bad89b6c34569533b8565d98ecd3770a486212e2
smcelvogue <78613740+smcelvogue@users.noreply.github.com>
2023-04-24 09:04:49 -0400

    Added database connection

--- backend/models/usermodel.js
+++ backend/models/usermodel.js
@@ -1,4 +1,19 @@
-const mongoose = require('mongoose')
+// Import the mongoose module
+const mongoose = require("mongoose");
+
+// Set `strictQuery: false` to globally opt into filtering by properties that aren't in the schema
+// Included because it removes preparatory warnings for Mongoose 7.
+// See: https://mongoosejs.com/docs/migrating_to_6.html#strictquery-is-removed-and-replaced-by-strict
+mongoose.set("strictQuery", false);
+
+// Define the database URL to connect to.
+const mongoDB = "mongodb://127.0.0.1:27017/workouts";
+
+// Wait for database to connect, logging an error if there is a problem
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
 
 const Schema = mongoose.Schema
 
@@ -16,6 +31,6 @@ const userSchema = new Schema({
         required: true
     },
     
-}, {timestamps: true}) //IDK if this line is right!!!
+}, {timestamps: true})
 
 module.exports = mongoose.model('User', userSchema)
