a8b44163998b7d121cdab4ed4cac7ebf7ee0b04d
smcelvogue <78613740+smcelvogue@users.noreply.github.com>
2023-04-24 09:00:32 -0400

    Added port to database connection

--- backend/models/workoutmodel.js
+++ backend/models/workoutmodel.js
@@ -7,7 +7,7 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
 // Define the database URL to connect to.
-const mongoDB = "mongodb://127.0.0.1/workouts";
+const mongoDB = "mongodb://127.0.0.1:27017/workouts";
 
 // Wait for database to connect, logging an error if there is a problem
 main().catch((err) => console.log(err));
