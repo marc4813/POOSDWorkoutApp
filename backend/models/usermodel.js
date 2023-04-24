0162fb73fcaa1ce19219f22ade0844ac727d7cc5
ilham0713 <88455600+ilham0713@users.noreply.github.com>
2023-04-24 16:19:37 -0400

    Update usermodel.js

--- backend/models/usermodel.js
+++ backend/models/usermodel.js
@@ -25,11 +25,7 @@ const userSchema = new Schema({
     lastName : {
         type: String,
         required: true
-    },
-    email : {
-        type: String,
-        required: true
-    },
+    }
     
 }, {timestamps: true})
 
