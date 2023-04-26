a90bdc72f6c87d3d04a4060e56b8af01e2cca1b0
smcelvogue <78613740+smcelvogue@users.noreply.github.com>
2023-04-26 12:40:40 -0400

    Removed DELETE and PATCH routes, fixed route paths

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,9 +2,7 @@ const { response } = require('express')
 const express = require('express')
 const {
     createUser,
-    getUser,
-    deleteUser,
-    updateUser
+    getUser
 } = require('../controllers/userController')
 
 
@@ -12,15 +10,9 @@ const {
 const router = express.Router()
 
 // GET a user
-router.get('/:id', getUser)
+router.get('/', getUser)
 
 // POST a user
 router.post('/', createUser)
-
-// DELETE a workout
-router.delete('/:id', deleteUser)
-
-// UPDATE a workout
-router.patch('/:id', updateUser)
 
 module.exports = router
