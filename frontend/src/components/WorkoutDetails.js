43433a2a459f40f6fc662ff9c08da517736a4ba3
smcelvogue <78613740+smcelvogue@users.noreply.github.com>
2023-04-28 08:43:44 -0400

    Added edit button
    The button works, except it isn't calling its handler, so no update occurs.

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,4 +1,6 @@
 import { useState, } from "react"
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 const WorkoutDetails = ({workout}) => {
 
     const [visible, setVisible] = useState(true);
@@ -28,6 +30,37 @@ const WorkoutDetails = ({workout}) => {
             setVisible((prev) => !prev);
         }
     }
+    
+    const handleUpdate = async (e) => {
+
+        console.log("we here")
+        const response = await fetch('api/workouts', {
+            method: 'PATCH',
+            body: {
+                "_id" : workout._id,
+                "title" : e.target.title,
+                "load" : e.target.load,
+                "reps" : e.target.reps
+            },
+            headers: {
+                'Content-Type' : 'application/json'
+            }
+        })
+
+        const json = await response.json()
+
+        if(!response.ok)
+        {
+            console.log("Cannot Update.")
+        }
+
+        // Set state for updated data
+        else
+        {
+            console.log("Update Successful. Reloading Components...")
+            setVisible((prev) => !prev);
+        }
+    }
 
     return (
         
@@ -38,6 +71,20 @@ const WorkoutDetails = ({workout}) => {
             {visible && (<p><strong>Reps: </strong>{workout.reps}</p>)}
             {visible && (<p>{workout.createdAt}</p>)}
             {visible && (<button onClick={handleDelete}>Delete Workout</button>)}
+            {visible && (<Popup trigger= {<button>Edit Workout</button>} position="top center">
+                <div>
+                    <form className="update" onSubmit={handleUpdate}>
+                        <h3>Update Workout</h3>
+                        <label>Exercise Title: </label>
+                        <input type="text" id="title" defaultValue={workout.title}/>
+                        <label>Load(in kg): </label>
+                        <input type="number" id="load" defaultValue={workout.load}/>
+                        <label>Reps: </label>
+                        <input type="number" id="reps" defaultValue={workout.reps}/>
+                        <button type="submit">Update Workout</button>
+                    </form>
+                </div>
+            </Popup>)}
         </div>
     )
 }
